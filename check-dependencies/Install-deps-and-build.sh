6013f81faac37a8424c91ac07324ead26a80ed3d
Dario Ramos <dario.fenix@gmail.com>
2017-07-05 18:01:52 -0300

    Embed .cpp files

--- check-dependencies/Install-deps-and-build.sh
+++ check-dependencies/Install-deps-and-build.sh
@@ -1,5 +1,5 @@
 #to use ./clone.sh ~/112bit/nuevo/
-#destination folder must exists
+#destination folder must exist
 
 declare -a BOOST_CHECK_RESULTS=("Success" "Headers not found" "Runtime error in header test" "Libraries not found" "Runtime error in library test")
 
@@ -36,28 +36,125 @@ function install_cmake
 	rm -rf cmake-3.7.0-rc1
 }
 
+function test_boost_headers
+{
+	tmpfile=$(mktemp "/tmp/boost-htest.XXXXXX.cpp")
+
+	echo '#include<iostream>
+#include<boost/any.hpp>
+#include <boost/version.hpp>
+#include <stdexcept>
+
+int main()
+{
+	try
+	{
+    	boost::any a(5);
+    	a = 7.67;
+    	double d = boost::any_cast<double>(a);
+    	std::cout << "Boost headers found! Boost version: " << BOOST_LIB_VERSION << std::endl;
+    	return 0;
+	}catch(std::exception e){
+		return -1;
+	}
+}' >> "$tmpfile"
+	test_exec=/tmp/boost_header_only_test
+	#if ! g++ "$tmpfile" -o "$test_exec" >&/dev/null; then
+	if ! g++ "$tmpfile" -o "$test_exec"; then
+		return 1
+	fi
+
+	if ! "$test_exec"; then
+		rm -rf "$test_exec"
+		return 2
+	fi
+	rm -rf "$test_exec"
+	rm "$tmpfile"
+	return 0
+}
+
+function test_boost_libraries
+{
+	tmpfile=$(mktemp "/tmp/boost-ltest.XXXXXX.cpp")
+
+	echo '#include<iostream>
+#include<boost/filesystem/operations.hpp>
+#include <stdexcept>
+
+namespace bfs=boost::filesystem;
+
+int main()
+{
+	try
+	{
+	    bfs::path p("clone_build_cint.sh");
+	    if(bfs::exists(p))
+	    {
+	    	std::cout<< "Boost libraries found!" <<std::endl;
+	        return 0;
+	    }
+	    return -1;
+	}catch(std::exception)
+	{
+		return -2;
+	}
+}' >> "$tmpfile"
+	#Link boost libraries against an executable, and run it
+	test_exec=/tmp/boost_library_test
+	if ! g++ "$tmpfile" -o "$test_exec" -lboost_filesystem -lboost_system >&/dev/null; then
+		return 1
+	fi
+
+	if ! "$test_exec"; then
+		rm -rf "$test_exec"
+		return 2
+	fi
+	rm -rf "$test_exec"
+	rm "$tmpfile"
+	return 0
+}
+
+function test_boost_fpic
+{
+	tmpfile=$(mktemp "/tmp/boost-ftest.XXXXXX.cpp")
+
+	echo '#include<iostream>
+#include<boost/filesystem/operations.hpp>
+#include <stdexcept>
+
+namespace bfs=boost::filesystem;
+
+class lib_class{
+public:
+	void foo(){
+	    bfs::path p("clone_build_cint.sh");
+	    if(!bfs::exists(p))
+	    {
+	    	throw std::runtime_error("Error in boost filesystem lib");
+	    }
+	}
+};' >> "$tmpfile"
+
+	g++ -g -ggdb -c "$tmpfile" -o /tmp/boost_ftest.o
+}
+
 function check_if_boost_installed
 {
-	#Check if it is possible to build a header-only Boost program
-	if ! g++ boost_header_only_test.cpp -o boost_header_only_test >&/dev/null; then
+	if ! test_boost_headers; then
 		echo ${BOOST_CHECK_RESULTS[1]}
 		return 1
 	fi
-	#Run the program and see if it works; also, get Boost version
-	if ! ./boost_header_only_test; then
+
+	if ! test_boost_libraries; then
 		echo ${BOOST_CHECK_RESULTS[2]}
 		return 2
 	fi
-	#Check if it possible to build a program using a Boost library
-	if ! g++ boost_library_test.cpp -o boost_library_test -lboost_filesystem -lboost_system >&/dev/null; then
+
+	if ! test_boost_fpic; then
 		echo ${BOOST_CHECK_RESULTS[3]}
 		return 3
 	fi
-	#Check if the program runs fine
-	if ! ./boost_library_test; then
-		echo ${BOOST_CHECK_RESULTS[4]}
-		return 4
-	fi
+
 	return 0
 }
 
@@ -89,7 +186,7 @@ function build_project
 
 function build_bitprim
 {
-	check_dependencies
+    check_dependencies
     build_project
 }
 
