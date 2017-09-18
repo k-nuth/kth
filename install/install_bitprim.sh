c5a5cb536392aaf318a1b18fe5fd749bee19c7b1
elfarto <arceri@pythian.com>
2017-09-18 14:58:31 -0400

    [skip ci] Use wget instead of curl to fetch pip

--- install/install_bitprim.sh
+++ install/install_bitprim.sh
@@ -7,7 +7,7 @@ INSTALL_CMD="sudo apt-get install -y"
 fi
 
 which cmake || $INSTALL_CMD cmake
-which pip || sudo url -s "https://bootstrap.pypa.io/get-pip.py"  | python
+which pip || sudo wget -qO- "https://bootstrap.pypa.io/get-pip.py"  | python
 pip install conan
 conan remote add bitprim https://api.bintray.com/conan/bitprim/bitprim
 wget -O conanfile.txt https://raw.githubusercontent.com/bitprim/bitprim/master/install/conanfile.txt
