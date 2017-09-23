75ba71b9176c78b06706e6da883751c89ce7d33f
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-09-23 08:50:44 -0300

    [skip ci] build script

--- build_win.bat
+++ build_win.bat
@@ -4,3 +4,11 @@ conan install ..
 cmake -G "Visual Studio 15 2017 Win64" -DWITH_TESTS=OFF  ..
 
 REM Open Visual Studio .sln file and Build the solution
+
+REM ---------------------------------
+REM Debug
+REM ---------------------------------
+
+conan install bitprim-conan-boost/1.64.0@bitprim/stable -s build_type=Debug
+conan install .. -s build_type=Debug
+cmake -G "Visual Studio 15 2017 Win64" -DWITH_TESTS=OFF  ..
