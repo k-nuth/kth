529a02035593dc5f330fd39e427e2bb0f7bb0edb
Fernando Pelliccioni <fpelliccioni@gmail.com>
2022-09-10 14:19:21 +0200

    misc

--- build_release_linux.sh
+++ build_release_linux.sh
@@ -4,10 +4,12 @@ mkdir -p build_release
 cd build_release
 conan install .. -s build_type=Release
 
-cmake .. -GNinja -DCMAKE_VERBOSE_MAKEFILE=ON \
+cmake .. -GNinja -DCMAKE_CXX_FLAGS=-Wno-deprecated-declarations \
+                 -DCMAKE_VERBOSE_MAKEFILE=ON \
                  -DUSE_CONAN=ON \
                  -DCURRENCY=BCH \
                  -DBINLOG=OFF \
+                 -DLOG_LIBRARY=spdlog \
                  -DWITH_CONSOLE_CAPI=ON \
                  -DDB_READONLY_MODE=OFF \
                  -DENABLE_TESTS=OFF \
@@ -15,13 +17,13 @@ cmake .. -GNinja -DCMAKE_VERBOSE_MAKEFILE=ON \
                  -DWITH_TOOLS=OFF \
                  -DENABLE_ECMULT_STATIC_PRECOMPUTATION=OFF \
                  -DDB_NEW=ON \
-                 -DDB_NEW_BLOCKS=ON \
-                 -DDB_NEW_FULL=ON \
-                 -DBUILD_NODE_EXE=OFF \
-                 -DBUILD_C_API=ON \
+                 -DDB_NEW_BLOCKS=OFF \
+                 -DDB_NEW_FULL=OFF \
+                 -DBUILD_NODE_EXE=ON \
+                 -DBUILD_C_API=OFF \
                  -DENABLE_SHARED_CAPI=ON \
                  -DCMAKE_BUILD_TYPE=Release
 
-ninja -j4
+ninja -j16
 # cmake --build .
 
